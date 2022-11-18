008d2006434ed4eb516db0ed3950c2a9327d8964
dkadur <dkadur7@outlook.com>
2022-11-17 19:04:28 -0500

    Updated XML file

--- services/device-service/src/main/java/com/dkadur/assettracker/deviceservice/entity/Device.java
+++ services/device-service/src/main/java/com/dkadur/assettracker/deviceservice/entity/Device.java
@@ -1,13 +1,9 @@
 package com.dkadur.assettracker.deviceservice.entity;
 
 import com.google.cloud.spring.data.spanner.core.mapping.*;
-import com.google.cloud.spring.data.spanner.core.mapping.Column;
-import com.google.cloud.spring.data.spanner.core.mapping.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 
 @Table(name="devices")
@@ -16,10 +12,8 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Device {
 
-    @Id
     @PrimaryKey(keyOrder = 1)
     @Column(name="deviceId")
-    //@GeneratedValue(strategy = GenerationType.AUTO)
     private Long deviceId;
 
     @Column(name="deviceName")
