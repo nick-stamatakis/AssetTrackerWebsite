4634485bddc631e89b497eb79b5907341e8e6871
dkadur <dkadur7@outlook.com>
2022-12-19 13:55:14 -0500

    Changed JPA Repository to Spanner for user service

--- services/user-service/src/main/java/com/dkadur/assettracker/userservice/repository/UserRepository.java
+++ services/user-service/src/main/java/com/dkadur/assettracker/userservice/repository/UserRepository.java
@@ -1,14 +1,13 @@
 package com.dkadur.assettracker.userservice.repository;
 
 import com.dkadur.assettracker.userservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.google.cloud.spring.data.spanner.repository.SpannerRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.PreRemove;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends SpannerRepository<User, Long> {
 
     List<User> findByUsername(String username);
 
