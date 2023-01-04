42f435cd717be4c8ad41acd506fd8364fb92b0c3
dkadur <dkadur7@outlook.com>
2023-01-04 17:48:54 -0500

    Deleted unnecessary frontend files

--- frontend/data.js
+++
@@ -1,9 +0,0 @@
-import { Chart } from "react-google-charts";
-
-<Chart
-  chartType="ScatterChart"
-  data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-  width="100%"
-  height="400px"
-  legendToggle
-/>
